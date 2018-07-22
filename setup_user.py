d3ba6b92688baa8a21c6ae7cff3e65ba52fa25bd
TeddyBearFuzzy <37773844+TeddyBearFuzzy@users.noreply.github.com>
2018-07-22 13:43:07 +0200

    Update setup_user.py
    Login and Register system bug fixes.

--- setup_user.py
+++ setup_user.py
@@ -2,12 +2,13 @@ import sys, os, time
 from user import User
 
 print "Welcome !"
-time.sleep(1)
+time.sleep(0.3)
 print "Do you want to login or register ? [l/r]\n\n"
 
 user_input = raw_input(">")
 
 if user_input == "r":
+    #Register
 
     start_again = True
 
@@ -22,24 +23,32 @@ if user_input == "r":
         user_input = raw_input(">")
         password = user_input
 
-        user = User(username, password)
-
-        if user.exists(username):
+        if User.exists(username):
             print "Username is already took :(\n"
-        start_again = True
+            start_again = True
+        else:
+            user = User(username, password)
+            user.save()
 
 
 if user_input == "l":
+    #Login
 
-    print "Enter your account name\n\n"
-    user_input = raw_input(">")
-    username = user_input
+    start_again = True
 
-    print "Enter your password\n\n"
-    user_input = raw_input(">")
-    password = user_input
+    while start_again:
+        start_again = False
+        print "Enter your account name\n\n"
+        user_input = raw_input(">")
+        username = user_input
 
-    user = User(username, password)
+        print "Enter your password\n\n"
+        user_input = raw_input(">")
+        password = user_input
 
-    if user.exists(username):
-        user.password_correct(password)
+        if User.password_correct(username, password):
+            print "Connected as %s !" % (username)
+        else:
+            print "Username or Password incorrect :("
+            
+            start_again = True
